8fbfa7ac802b76f122fdf3aa17584483edfe27b2
Keonbe <Keonbe@users.noreply.github.com>
2025-12-07 20:22:49 +0800

    Add axios configuration and enhance database connection script with comments for clarity

---
+++ Post-React-Migration/pwd-application-system/src/api/axiosConfig.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const api = axios.create({
+  // Updated baseURL to match project location under htdocs/XAMPP
+  baseURL:
+    "http://localhost/webdev_finals/PWD AUTOMATED APPLICATION SYSTEM/PWD-Automated-Application-System/Post-React-Migration/xampp-php-mysql-files/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default api;
+
+  // To be read as(see example, enclose in brackets `{}`):
+  // C:\Users\admin\xampp\8.2.12-0\htdocs\{webdev_finals\PWD AUTOMATED APPLICATION SYSTEM\PWD-Automated-Application-System\Post-React-Migration\xampp-php-mysql-files/api}
--- Post-React-Migration/xampp-php-mysql-files/config.php
+++ Post-React-Migration/xampp-php-mysql-files/config.php
@@ -1,9 +1,15 @@
 <?php
+// Database configuration
 $servername = "localhost";
 $username = "root";
+
+// no password for xampp by default
 $password = "";
+
+// Database name
 $dbname = "PWDRegistry";
 
+// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
